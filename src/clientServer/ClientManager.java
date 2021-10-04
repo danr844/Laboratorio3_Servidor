9cdb74158293316eb5f89efd505fc37497f00e02
ee-ortiz <ee.ortiz@uniandes.edu.co>
2021-10-04 13:32:31 -0500

    numero de clientes definido

--- src/clientServer/ClientManager.java
+++ src/clientServer/ClientManager.java
@@ -1,10 +1,17 @@
 package clientServer;
 
+import java.util.Scanner;
+
 public class ClientManager {
 	
 	public static void main(String[] args) {
 		
-		for(int i =0; i<25; i++) {
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Por favor ingrese el número de clientes que se desea conectar: ");
+	    
+	    int numClientes = Integer.parseInt(myObj.nextLine());  // Read user input
+		
+		for(int i =0; i<numClientes; i++) {
 			Thread thread = new ClientThread();
 			thread.run();
 		}
